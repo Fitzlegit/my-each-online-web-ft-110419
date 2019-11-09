fbe3e745e571bc07774cb1f7b0fe241f4eee48d7
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-09 22:07:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ def my_each(array) # put argument(s) here
   # code here
   i = 0
   
-  while i < array.array
+  while i < array.length
     yield (array[i])
-    array
     i += 1
   end
+  array
 end
